abcd249a694338e629716dc699cbc7b21681d893
Nerijus <nerijusgood@gmail.com>
2016-09-13 17:43:57 +0200

    Example component generated

---
+++ app/components/Button/index.js
@@ -0,0 +1,20 @@
+/**
+*
+* Button
+*
+*/
+
+import React from 'react';
+import styles from './styles.css';
+
+class Button extends React.Component { // eslint-disable-line react/prefer-stateless-function
+  render() {
+    return (
+      <button className={styles.button}>
+        Test button - please
+      </button>
+    );
+  }
+}
+
+export default Button;
---
+++ app/components/Button/stories/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { storiesOf, action } from '@kadira/storybook';
+import Button from '../';
+
+storiesOf('Button', module)
+  .add('with text', () => (
+    <Button onClick={action('clicked')}>My First Button</Button>
+  ))
+  .add('with no text', () => (
+    <Button></Button>
+  ));
