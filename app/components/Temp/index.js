5ab6398e86eacedac81a3c746d3f78eac497ceda
Nerijus <nerijusgood@gmail.com>
2016-09-16 11:57:34 +0200

    Updated styling

--- app/components/Temp/index.js
+++ app/components/Temp/index.js
@@ -12,35 +12,38 @@ import Granim from 'granim'
 
 class Temp extends React.Component { // eslint-disable-line react/prefer-stateless-function
 
-  // componentDidMount() {
-  //   this.handleBgEffects()
-  // }
-  //
-  // handleBgEffects() {
-  //   this.granimInstance = new Granim({
-  //     element: '#canvas-basic',
-  //     name: 'basic-gradient',
-  //     direction: 'left-right',
-  //     opacity: [1, 1],
-  //     isPausedWhenNotInView: true,
-  //     states: {
-  //       'default-state': {
-  //         gradients: [
-  //           ['#AA076B', '#61045F'],
-  //           ['#02AAB0', '#00CDAC'],
-  //           ['#DA22FF', '#9733EE'],
-  //         ],
-  //       },
-  //     },
-  //   })
-  // }
+  componentDidMount() {
+    this.handleBgEffects()
+  }
+
+  handleBgEffects() {
+    this.granimInstance = new Granim({
+      element: '#canvas-basic',
+      name: 'basic-gradient',
+      direction: 'left-right',
+      opacity: [1, 1],
+      isPausedWhenNotInView: true,
+      states: {
+        'default-state': {
+          gradients: [
+            ['#AA076B', '#61045F'],
+            ['#02AAB0', '#00CDAC'],
+            ['#DA22FF', '#9733EE'],
+          ],
+        },
+      },
+    })
+  }
 
   render() {
     return (
-      <div id='canvas-basic' className={styles.Temp}>
-        {/* <FormattedMessage {...messages.header} /> */}
+      <div>
+        <canvas id='canvas-basic' className={styles.Temp}>
+          {/* <FormattedMessage {...messages.header} /> */}
+        </canvas>
+
         <div className={styles.Content}>
-          Hello
+          <h1>Hello</h1>
         </div>
       </div>
     )
